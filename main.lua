818b7c287b69d9a980875052a419d2fb2b1fea10
Thomas de Groot <thomas@altenter.nl>
2024-01-28 16:25:36 +0100

    Timelimit and bg color

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ GGJ game main
 ]]--
 
 -- config
-time_limit=30
+time_limit=999
 n_humor_types=4
 n_cities=4
 
@@ -26,15 +26,15 @@ end
 
 function _draw()
 	-- background
-	cls(2)
+	cls()
 	
 	-- scene
 	scene.current:draw()
 
 	-- ui
-	rect(4,12,123,123,13)
-	prints("score:"..pad(score.."00",8),4,4,7)
-	prints("level:"..level,65,4,7)
-	prints("time:"..pad(ceil(timer/30),2),97,4,7)
+	rect(0,0,127,127,13)
+	prints("score:"..pad(score,6),4,4,7)
+	prints("level:"..level,59,4,7)
+	prints("time:"..pad(ceil(timer/30),3),93,4,7)
 end
 
